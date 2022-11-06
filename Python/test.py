8f3553b5d5c87516336114bb09b7c1af4384f9a8
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-11-05 22:11:38 -0500

    cliente
    ddddd

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,21 @@
-print("Amogus")
+import socket
+import sys
+ip='localhost'
+port=8000
+
+mi_socket=socket.socket()
+mi_socket.connect((ip,port))
+
+while True:
+    mensaje=input()
+    if mensaje!='adios':
+        mi_socket.send(mensaje)
+        respuesta=mi_socket.recv(4000)
+        print(respuesta)
+    else:
+        mi_socket.send(mensaje)
+        mi_socket.close()
+        sys.exit()
+
+
+
