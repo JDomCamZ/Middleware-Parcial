329916c9fd5769796b48d02e0f945fa554e7e0bb
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-11-05 22:33:39 -0500

    push consuming python

--- Python/test.py
+++ Python/test.py
@@ -1,16 +1,16 @@
 import socket
 import sys
-ip='localhost'
-port=8000
+ip='192.168.0.18'
+port=4444
 
 mi_socket=socket.socket()
 mi_socket.connect((ip,port))
 
 while True:
-    mensaje=input()
+    mensaje=input(str)
     if mensaje!='adios':
-        mi_socket.send(mensaje)
-        respuesta=mi_socket.recv(4000)
+        mi_socket.send(mensaje.encode())
+        respuesta=mi_socket.recv(4000).decode()
         print(respuesta)
     else:
         mi_socket.send(mensaje)
