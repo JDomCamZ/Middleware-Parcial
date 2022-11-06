00470828ed4a6a01d038db6d3ddf3e9590f1c8b2
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-11-06 03:06:18 -0500

    mandar mensaje python

--- Python/Consuming1.py
+++ Python/Consuming1.py
@@ -3,7 +3,7 @@ import sys
 ip='192.168.0.18'
 port=4444
 
-mi_socket=socket.socket()
+mi_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mi_socket.connect((ip,port))
 
 print("Esperando mensaje de Producing")
--- Python/test.py
+++ Python/test.py
@@ -3,13 +3,14 @@ import sys
 ip='192.168.0.18'
 port=4444
 
-mi_socket=socket.socket()
+mi_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mi_socket.connect((ip,port))
 
 while True:
     mensaje=input(str)
+    mensaje+='\n'
     mi_socket.send(mensaje.encode())
-    if mensaje!="adios":
+    if mensaje!="adios\n":
         respuesta=mi_socket.recv(4000).decode()
         print(respuesta)
     else:
