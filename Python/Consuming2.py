3af21055eac005fb9c00c24ab6b3b9a2a20c1bcd
MiguelHRLX <miguel.huamani.r@uni.pe>
2022-11-06 19:19:33 -0500

    timer python

--- Python/Consuming2.py
+++ Python/Consuming2.py
@@ -1,16 +1,24 @@
 import socket
 import sys
+import time
+
 ip='192.168.0.18'
 port=4444
 
 mi_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mi_socket.connect((ip,port))
 
+def cron():
+    time.sleep(3)
+    print("Listo")
+ 
+
 while True:
     mensaje=input(str)
     mensaje+='\n'
     mi_socket.send(mensaje.encode())
     if mensaje!="adios\n":
+        cron()
         respuesta=mi_socket.recv(4000).decode()
         print(respuesta)
     else:
